bf4f41822ed4208e94d20a36a7fed4a2f9866e65
shireens <shireens@my.yorku.ca>
2023-02-09 12:41:21 -0500

    Removed hello button

--- src/main/java/com/example/hello/BookmarkController.java
+++ src/main/java/com/example/hello/BookmarkController.java
@@ -2,15 +2,17 @@ package com.example.hello;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ChoiceBox;
 
 public class BookmarkController {
     @FXML
     private Label welcomeText;
 
+
+
     @FXML
-    protected void onHelloButtonClick() {
+    protected void  onSearchButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
-
 
 }
