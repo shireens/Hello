aac2333a7b6c3dd3928a57285040e745ec55e419
EthanFifle <ethan.fifle@gmail.com>
2023-02-10 08:37:53 -0500

    Update V3

--- src/main/java/com/example/hello/BookmarkController.java
+++ src/main/java/com/example/hello/BookmarkController.java
@@ -2,24 +2,18 @@ package com.example.hello;
 import com.example.hello.backend.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
-
 import java.util.Set;
 
 public class BookmarkController {
     @FXML
-    private Label welcomeText;
-
-    @FXML
-    private Label testText;
+    private Label searchingText;
 
     @FXML
     private ChoiceBox<String> firstChoiceBox;
@@ -27,7 +21,7 @@ public class BookmarkController {
 
     @FXML
     private ChoiceBox<String> secondChoiceBox;
-    private String searchBy = ""; //Genre, Actor eyc.
+    private String searchBy = ""; //Genre, Actor etc.
 
     @FXML
     private TextField searchText;
@@ -38,8 +32,18 @@ public class BookmarkController {
     private ObservableList<String> items = FXCollections.observableArrayList();
 
     @FXML
-    private void initialize() {
+    private ListView<String> myBookList;
+    private ObservableList<String> bookList = FXCollections.observableArrayList();
+
+    @FXML
+    private ListView<String> myMovieList;
+    private ObservableList<String> movieList = FXCollections.observableArrayList();
+
+    @FXML
+    private void initialize() { //Initializes all Listview items
         myListView.setItems(items);
+        myBookList.setItems(bookList);
+        myMovieList.setItems(movieList);
     }
 
     @FXML
@@ -47,61 +51,63 @@ public class BookmarkController {
 
         items.clear();
         searchString = searchText.getText();
-        welcomeText.setTextFill(Color.WHITE);
+        searchingText.setTextFill(Color.WHITE);
         MovieSearch ms = new MovieSearch();
         BookSearch bs = new BookSearch();
 
-        if(searchType.equals("Movies") || searchType.equals("")){ // first Pain
+        if(searchType.equals("Movies")){ // first Pain
 
-            if(searchBy.equals("Movie-Search by Title") || searchBy.equals("")){
-                welcomeText.setText("Movie-Search by Title: " + searchString);
+            if(searchBy.equals("Movie-Search by Title")){
+                searchingText.setText("Searching Movies by Title: " + searchString + "...");
 
                 Set<Movie> arr = ms.MovieByTitle(searchString);
                 for(Movie m : arr){
                     items.add(m.getTitle());
                 }
-            }else if(secondChoiceBox.getValue().equals("Movie-Search by Actor")){
-                welcomeText.setText("Movie-Search by Actor: " + searchString);
+            }else if(searchBy.equals("Movie-Search by Actor")){
+                searchingText.setText("Searching Movies by Actor: " + searchString + "...");
                 Set<Movie> arr = ms.MovieByActor(searchString);
                 for(Movie m : arr){
                     items.add(m.getTitle());
                 }
 
             }else{
-                //Please choose Movie-Search by Genre or Movie-Search by Actor
+                searchingText.setTextFill(Color.RED);
+                searchingText.setText("Please choose a \"Movie-\" selection from the drop down title \"Search by\" ");
             }
 
         }else if (searchType.equals("Books")){
 
-            if(searchBy.equals("Book-Search by Title") || searchBy.equals("")){
-                welcomeText.setText("Book-Search by Title: " + searchString);
+            if(searchBy.equals("Book-Search by Title")){
+                searchingText.setText("Searching Books by Title: " + searchString + "...");
 
                 Set<Book> arr = bs.searchBookName(searchString);
                 for(Book b : arr){
                     items.add(b.getTitle());
                 }
             }else if(searchBy.equals("Book-Search by Genre")){
-                welcomeText.setText("Book-Search by Genre: " + searchString);
+                searchingText.setText("Searching Books by Genre: " + searchString +  "...");
 
                 Set<Book> arr = bs.SearchBookGenre(searchString);
                 for(Book b : arr){
                     items.add(b.getTitle());
                 }
             }else if (searchBy.equals("Book-Search by Author")){
-                welcomeText.setText("Book-Search by Author: " + searchString);
+                searchingText.setText("Searching Books by Author: " + searchString + "...");
 
                 Set<Book> arr = bs.searchBookAuthor(searchString);
                 for(Book b : arr){
                     items.add(b.getTitle());
                 }
             }else{
-                //Please choose Book-Search by Title/Author/Genre
+                searchingText.setTextFill(Color.RED);
+                searchingText.setText("Please choose a \"Book-\" selection from the drop down title \"Search by\" ");
             }
         }else {
-            // Please select search criteria (Movies/Books) and ("Movie-Search by Genre",
-            //                                                   "Movie-Search by Actor",
-            //                                                   "Book-Search by Genre",
-            //                                                   "Book-Search by Author").
+
+            searchingText.setTextFill(Color.RED);
+            searchingText.setText("Please choose a selection from the drop down title \"Type\" and \"Search by\" ");
+
         }
     }
 
@@ -114,7 +120,52 @@ public class BookmarkController {
     private void onSecondChoiceClick(ActionEvent event){
         searchBy = secondChoiceBox.getValue();
     }
-    //String lineText = text.getText().substring(line.getStart(), line.getStart() + line.getLength());
-    //String lineText = seachText.getText().substring(line.getStart(), line.getStart() + line.getLength());
+
+    @FXML
+    private void saveToList(ActionEvent event){
+
+        final String selectedItem = myListView.getSelectionModel().getSelectedItem();
+        if(searchType.equals("Books")){
+            bookList.add(selectedItem);
+        }else if(searchType.equals("Movies")){
+            movieList.add(selectedItem);
+        }else{
+            //error checking
+        }
+    }
+
+    @FXML
+    private void addBookToFavourites(ActionEvent event){
+        final String selectedItem = myBookList.getSelectionModel().getSelectedItem();
+        final int selectedIdx = myBookList.getSelectionModel().getSelectedIndex();
+        myBookList.getItems().remove(selectedIdx);
+        bookList.add(0, "*" + selectedItem); //Pushes favourite items to top of the list
+
+    }
+
+    @FXML
+    private void addMovieToFavourites(ActionEvent event){
+        final String selectedItem = myMovieList.getSelectionModel().getSelectedItem();
+        final int selectedIdx = myMovieList.getSelectionModel().getSelectedIndex();
+        myMovieList.getItems().remove(selectedIdx);
+        movieList.add(0,"*" + selectedItem);//Pushes favourite items to top of the list
+    }
+
+    @FXML
+    private void removeBook(ActionEvent event){
+
+        final int selectedIdx = myBookList.getSelectionModel().getSelectedIndex();
+        if (selectedIdx != -1) {
+            myBookList.getItems().remove(selectedIdx);
+        }
+    }
+
+    @FXML
+    private void removeMovie(ActionEvent event){
+        final int selectedIdx = myMovieList.getSelectionModel().getSelectedIndex();
+        if (selectedIdx != -1) {
+            myMovieList.getItems().remove(selectedIdx);
+        }
+    }
 
 }
