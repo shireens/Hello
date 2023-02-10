d1574c2eba8055f7a234e95bdeeb9463eeb9e388
EthanFifle <ethan.fifle@gmail.com>
2023-02-09 21:30:03 -0500

    Update V2

--- src/main/java/com/example/hello/BookmarkController.java
+++ src/main/java/com/example/hello/BookmarkController.java
@@ -16,9 +16,11 @@ public class BookmarkController {
 
     @FXML
     private ChoiceBox<String> firstChoiceBox;
+    private String searchType = ""; //Movies or Books
 
     @FXML
     private ChoiceBox<String> secondChoiceBox;
+    private String searchBy = ""; //Genre, Actor eyc.
 
    /* @FXML
     protected void  onSearchButtonClick() {
@@ -26,37 +28,50 @@ public class BookmarkController {
     }*/
 
     @FXML
-
     private TextField searchText;
+    private String searchString = "";
 
     @FXML
     private void onSearchButtonClick(ActionEvent event) {
-        //searchText.getText();
+
+        searchString = searchText.getText();
         welcomeText.setTextFill(Color.WHITE);
-        if(firstChoiceBox.getValue().equals("Movies") || firstChoiceBox.getValue().equals("Movies")){ // first Pain
-            if(secondChoiceBox.getValue().equals("Movie-Search by Genre")){
-                welcomeText.setText("Movie-Search by Genre: " + searchText.getText() + "!!!!");
-            }else {
-                welcomeText.setText("Movie-Search by Other: " + searchText.getText() + "!!!!");
+
+        if(searchType.equals("Movies") || searchType.equals("")){ // first Pain
+
+            if(searchBy.equals("Movie-Search by Genre") || searchBy.equals("")){
+                welcomeText.setText("Movie-Search by Genre: " + searchString);
+            }else if(secondChoiceBox.getValue().equals("Movie-Search by Actor")){
+                welcomeText.setText("Movie-Search by Actor: " + searchString);
+            }else{
+                //Please choose Movie-Search by Genre or Movie-Search by Actor
             }
 
-        }else if (firstChoiceBox.getValue().equals("Books")){
-            welcomeText.setText("Book search: " + searchText.getText() + "!!!!");
+        }else if (searchType.equals("Books")){
+
+            if(searchBy.equals("Book-Search by Genre") || searchBy.equals("")){
+                welcomeText.setText("Book-Search by Genre: " + searchString);
+            }else if(secondChoiceBox.getValue().equals("Book-Search by Author")){
+                welcomeText.setText("Book-Search by Author: " + searchString);
+            }else{
+                //Please choose Book-Search by Genre or Book-Search by Author
+            }
         }else {
-            // Error check
+            // Please select search criteria (Movies/Books) and ("Movie-Search by Genre",
+            //                                                   "Movie-Search by Actor",
+            //                                                   "Book-Search by Genre",
+            //                                                   "Book-Search by Author").
         }
     }
 
     @FXML
     private void onChoiceClick(ActionEvent event){
-        testText.setText("Selected item: " + firstChoiceBox.getValue() + "!!!!");
-        testText.setTextFill(Color.RED);
+        searchType = firstChoiceBox.getValue();
     }
 
     @FXML
     private void onSecondChoiceClick(ActionEvent event){
-        testText.setText("Selected item: " + secondChoiceBox.getValue() + "!!!!");
-        testText.setTextFill(Color.BLUE);
+        searchBy = secondChoiceBox.getValue();
     }
     //String lineText = text.getText().substring(line.getStart(), line.getStart() + line.getLength());
     //String lineText = seachText.getText().substring(line.getStart(), line.getStart() + line.getLength());
