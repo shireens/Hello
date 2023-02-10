9326885683398ab004acb59270b6ee5dbfbb08a3
EthanFifle <ethan.fifle@gmail.com>
2023-02-10 10:40:36 -0500

    Update V3

--- src/main/java/com/example/hello/BookmarkApp.java
+++ src/main/java/com/example/hello/BookmarkApp.java
@@ -3,9 +3,6 @@ package com.example.hello;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
